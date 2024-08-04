7703b162c30ef7a653a68990c07477378515e586
student <kmac.kimaru93@gmail.com>
2024-08-04 20:05:20 +0300

    aligning navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Nav, NavDropdown } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
 import "./Navbar.css";
 
 function Navbar() {
