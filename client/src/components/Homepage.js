5f1a19e69ce3468009dc7489b5e2158a27744537
Kamau <kamauwamuigaijr@gmail.com>
2024-08-05 09:58:49 +0300

    “Homepagefix”

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,23 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
-import "./Homepage.css"; // Importing the CSS file for the homepage
-
+import "./Homepage.css"; 
 function Homepage() {
   return (
     <div className="homepage">
       <Navbar />
-      <header className="homepage-header">
-        <h1>Welcome to SendIT</h1>
-        <p>Your trusted courier service platform</p>
-      </header>
-      <section className="homepage-content">
-        <h2>Features</h2>
-        <ul>
-          <li>Create and track parcels</li>
-          <li>Manage delivery destinations</li>
-          <li>Admin and user specific functionalities</li>
-        </ul>
-      </section>
+      <div className="homepage-content">
+        <h1 className="welcome-message">Welcome to SendIT</h1>
+        <p className="sub-message">Your trusted Courier service for seamless parcel deliveries.</p>
+        <button className="send-parcel-button">Send a Parcel</button>
+      </div>
     </div>
   );
 }
