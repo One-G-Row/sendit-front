1f1432c3cfb42a5e86e197b38d1069dd8dc6811d
Kamau <kamauwamuigaijr@gmail.com>
2024-08-05 10:08:57 +0300

    “FIXEDHomepagealignment”

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
-import "./Homepage.css"; 
+import "./Homepage.css";
+
 function Homepage() {
   return (
     <div className="homepage">
       <Navbar />
-      <div className="homepage-content">
-        <h1 className="welcome-message">Welcome to SendIT</h1>
-        <p className="sub-message">Your trusted Courier service for seamless parcel deliveries.</p>
-        <button className="send-parcel-button">Send a Parcel</button>
+      <div className="content">
+        <h1 className="title">Welcome to SendIT</h1>
+        <p className="subtitle">Your trusted Courier service for seamless parcel deliveries.</p>
+        <button className="button">Send a Parcel</button>
       </div>
     </div>
   );
