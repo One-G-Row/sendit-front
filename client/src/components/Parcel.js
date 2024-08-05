fa2be8f4393e5092d31f01d68246d0e6297f1e8e
Kamau <kamauwamuigaijr@gmail.com>
2024-08-05 12:38:33 +0300

    “FIXEDdoubleNavbar”

--- client/src/components/Parcel.js
+++ client/src/components/Parcel.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 function Parcel() {
   return (
     <div className="parcel">
-      <Navbar />
       <h1>Parcel</h1>
     </div>
   );
