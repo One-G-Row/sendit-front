5d20710cea1287b73488b8e0f689d99a4c7a8b59
Charles <charlesmuteithia@gmail.com>
2024-08-06 14:11:17 +0300

    creates content for user login component

--- client/src/components/LoginUser.js
+++ client/src/components/LoginUser.js
@@ -1,13 +1,51 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React, { useState } from 'react';
+import './LoginUser.css';
 
-function LoginUser() {
+const LoginUser = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Handle user login logic here
+    console.log('User Email:', email);
+    console.log('User Password:', password);
+  };
+
   return (
-    <div>
-      <Navbar />
-      <h1>Login User</h1>
+    <div className="login-user-container">
+      <h2 className="login-user-title">Login User</h2>
+      <form className="login-user-form" onSubmit={handleLogin}>
+        <div className="login-user-form-group">
+          <label htmlFor="email" className="login-user-label">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="login-user-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="login-user-form-group">
+          <label htmlFor="password" className="login-user-label">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="login-user-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button className="login-user-button" type="submit">Log in</button>
+      </form>
+      <div className="login-user-footer">
+        <a href="#" className="login-user-link">Forgot password?</a>
+        <a href="./SignupUser" className="login-user-link">Sign up</a>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginUser;
